f83c023448130b56a274832bae50c8f0de83e020
DTirthankar <tirthankardebnathtd@gmail.com>
2025-01-29 21:37:38 +0530

    Add files via upload

---
+++ 01_Problem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+// Which of the following is invalid in c ?
+
+    int a = 1;
+    int b = a;
+    int v = 3 * 3;
+    // char dt ='21 dec 2024';WRONG!
+    return 0;
+}
